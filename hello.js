d464321a36c3e3c37a3177a78808fca5e5d8002b
Ricardofelipecg <Ricardofelipecg@gmail.com>
2024-01-14 13:12:21 -0300

    commit

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hola Mundo, les saludo desde Santiago de Chile!";
 console.log(hello());
 exports.hello = hello;
